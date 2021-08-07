f3e5175de308dbfa64247fc91cabb86b89181188
Yusuf İpek <yusufipek@protonmail.com>
2021-08-07 15:11:26 +0300

    (create) created searchbar component and styled it

--- commons/home.js
+++ commons/home.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import Searchbar from "../components/searchbar";
 export default function Home({ posts }) {
+  const [search, setSearch] = useState("");
+  const filteredPosts =
+    search.length === 0
+      ? posts
+      : posts.filter((post) =>
+          post.title.toLowerCase().includes(search.toLowerCase())
+        );
+
   return (
     <div className="homepage">
+      <Searchbar search={search} setSearch={setSearch} />
+
       <h1 className="homepage_heading-1">Rehberler</h1>
       <div className="homepage_posts">
-        {posts.slice(0, 20).map((post) => (
+        {filteredPosts.slice(0, 20).map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </div>
