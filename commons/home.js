df8e941c9f4d2afcd97a6b62c8d227a663d534ae
Yusuf İpek <yusufipek@protonmail.com>
2021-08-07 14:52:17 +0300

    (create) created dummy posts and styled it

--- commons/home.js
+++ commons/home.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div className="homepage"></div>;
+export default function Home({ posts }) {
+  return (
+    <div className="homepage">
+      <h1 className="homepage_heading-1">Rehberler</h1>
+      <div className="homepage_posts">
+        {posts.slice(0, 20).map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Home from "../commons/home";
 
-export default function App() {
+export default function App({ posts }) {
   return (
     <div>
       <Head>
@@ -12,7 +12,18 @@ export default function App() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Home />
+      <Home posts={posts} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
